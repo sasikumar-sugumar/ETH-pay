b3d8d329bea16617cd5897f3b722e0c86fcabe9a
SASIKUMAR SUGUMAR <sasisugumar@gmail.com>
2017-08-04 21:38:53 -0500

    Initial Commit

---
+++ public/js/config/app.js
@@ -0,0 +1,20 @@
+var ETH-pay = angular.module('ETH-pay', ['ngMaterial', 'ngAnimate', 'ngMessages', 'ngAria', 'ui.router']);
+
+(function(app) {
+    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider.state('home', {
+            url: '/',
+            templateUrl: 'partials/home-partial.html',
+            controller: 'HomeController'
+        })
+
+        .state('about', {
+            url: '/about',
+            templateUrl: 'partials/about-partial.html',
+            controller: 'AboutController'
+        });
+    }]);
+})(ETH-pay);
